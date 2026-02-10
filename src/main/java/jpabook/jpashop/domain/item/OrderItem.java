05d57d67942edc66968c1b5b850f6315a9eb7ac5
seunggi <hsg77689469@gmail.com>
2026-02-10 22:05:23 +0900

    주문, 주문상품 엔티티, 주문 리포지토리 개발

--- src/main/java/jpabook/jpashop/domain/item/OrderItem.java
+++ src/main/java/jpabook/jpashop/domain/item/OrderItem.java
@@ -2,6 +2,7 @@ package jpabook.jpashop.domain.item;
 
 import lombok.Getter;
 import lombok.Setter;
+import jpabook.jpashop.domain.item.Item;
 import jakarta.persistence.*;
 @Entity
 @Table(name = "order_item")
@@ -18,4 +19,24 @@ public class OrderItem {
     private Order order; //주문
     private int orderPrice; //주문 가격
     private int count; //주문 수량
+    //==생성 메서드==//
+    public static OrderItem createOrderItem(Item item, int orderPrice, int
+            count) {
+        OrderItem orderItem = new OrderItem();
+        orderItem.setItem(item);
+        orderItem.setOrderPrice(orderPrice);
+        orderItem.setCount(count);
+        item.removeStock(count);
+        return orderItem;
+    }
+    //==비즈니스 로직==//
+    /** 주문 취소 */
+    public void cancel() {
+        getItem().addStock(count);
+    }
+    //==조회 로직==//
+    /** 주문상품 전체 가격 조회 */
+    public int getTotalPrice() {
+        return getOrderPrice() * getCount();
+    }
 }
