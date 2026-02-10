f9d4541e8406a20db95759bdd653fc7ffae0403b
seunggi <hsg77689469@gmail.com>
2026-02-11 00:25:04 +0900

    상품 목록

--- src/main/java/jpabook/jpashop/web/ItemController.java
+++ src/main/java/jpabook/jpashop/web/ItemController.java
@@ -12,11 +12,13 @@ import java.util.List;
 @RequiredArgsConstructor
 public class ItemController {
     private final ItemService itemService;
+
     @GetMapping(value = "/items/new")
     public String createForm(Model model) {
         model.addAttribute("form", new BookForm());
         return "items/createItemForm";
     }
+
     @PostMapping(value = "/items/new")
     public String create(BookForm form) {
         Book book = new Book();
@@ -27,5 +29,16 @@ public class ItemController {
         book.setIsbn(form.getIsbn());
         itemService.saveItem(book);
         return "redirect:/items";
+    }
+
+    /**
+     * 상품 목록
+     */
+    @GetMapping(value = "/items")
+    public String list(Model model) {
+        List<Item> items = itemService.findItems();
+        model.addAttribute("items", items);
+        return "items/itemList";
+
     }
 }
